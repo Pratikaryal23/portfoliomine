38582479316ac505f10f93a8502aef203e5a1a73
Pratikaryal23 <aryalpartik057@gmail.com>
2025-02-14 23:38:59 +0545

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import Contact from './Components/Home/Contact'
 
 const page = () => {
   return (
-    <div className='flex flex-col gap-2'>
+    <div className='flex flex-col gap-[0.5px]'>
       <Home/>
       <About />
         <SkillsCard/>
